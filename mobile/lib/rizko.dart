d1345cddd6fdb9560925b4215675f413abef4fc3
rzkoafrynnn <rzkoafrynnn@gmail.com>
2026-05-20 13:46:45 +0700

    mengerjakan halaman wishlist Fixes #6

---
+++ mobile/lib/rizko.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rizko extends StatefulWidget {
+  const Rizko({super.key});
+
+  @override
+  State<Rizko> createState() => _RizkoState();
+}
+
+class _RizkoState extends State<Rizko> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
