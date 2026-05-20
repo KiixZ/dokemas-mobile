e2cbcdcf83b4b8f9515103e04534b3d2ea8bb58a
Rifki Saputra <sptrkii@gmail.com>
2026-05-20 13:47:09 +0700

    ripki kerjain 1

---
+++ mobile/lib/ripki.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ripki extends StatefulWidget {
+  const ripki({super.key});
+
+  @override
+  State<ripki> createState() => _ripkiState();
+}
+
+class _ripkiState extends State<ripki> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
