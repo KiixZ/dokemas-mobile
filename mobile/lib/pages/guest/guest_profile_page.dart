12a9b4c7092a3397100cdb7b069f232b9b2db774
slsblts <salsabilat625@gmail.com>
2026-06-15 22:53:59 +0700

    menambahkan halaman guest profile wishlist itinerary

---
+++ mobile/lib/pages/guest/guest_profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../../widgets/guest_login_required_page.dart';
+
+class GuestProfilePage extends StatelessWidget {
+  const GuestProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GuestLoginRequiredPage(
+      icon: Icons.favorite_border,
+      title: 'Profile Saya',
+      description:
+          'Kelola akun dan aktivitas wisatamu\ndengan mudah.',
+      selectedIndex: 4,
+    );
+  }
+}
